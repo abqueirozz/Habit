58e5981ec7cff112e79fd746a24267b837a5c2ac
Arthur Queiroz <abqbisneto@gmail.com>
2023-09-17 20:10:04 -0300

    refact bottomSheet

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -1,4 +1,4 @@
-import { useEffect, ReactNode} from "react";
+import { useEffect, ReactNode, useCallback } from "react";
 import { View, StyleSheet, Dimensions, Text } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import Animated, { useAnimatedStyle, useSharedValue, withSpring, withTiming } from "react-native-reanimated";
@@ -7,13 +7,19 @@ const { height: SCREEN_HEIGHT } = Dimensions.get('window')
 const MAX_TRANSLATION_Y = -SCREEN_HEIGHT + 50
 const MIN_TRANSLATION_Y = -SCREEN_HEIGHT / 8
 
-interface BottomSheetProps{
+interface BottomSheetProps {
     children?: ReactNode
 }
 
-export const BottomSheet = ({children}:BottomSheetProps) => {
+export const BottomSheet = ({ children }: BottomSheetProps) => {
     const TY = useSharedValue(0)
     const context = useSharedValue({ y: 0 })
+
+    const srollTo = useCallback((dimension: number) => {
+        'worklet';
+        TY.value = withSpring(dimension, { damping: 50 })
+    }, [])
+
     const gesture = Gesture.Pan().onStart(() => {
         context.value = { y: TY.value }
     })
@@ -24,9 +30,9 @@ export const BottomSheet = ({children}:BottomSheetProps) => {
         })
         .onEnd(() => {
             if (TY.value <= MAX_TRANSLATION_Y) {
-                TY.value = withSpring(-SCREEN_HEIGHT +100 , { damping: 50 })
+                srollTo(-SCREEN_HEIGHT + 100)
             } else if (TY.value > MIN_TRANSLATION_Y) {
-                TY.value = withSpring(MIN_TRANSLATION_Y , { damping: 50 })
+                srollTo(MIN_TRANSLATION_Y)
             }
         })
 
@@ -37,18 +43,18 @@ export const BottomSheet = ({children}:BottomSheetProps) => {
     }))
 
     useEffect(() => {
-        TY.value = withSpring(MIN_TRANSLATION_Y, { damping: 50 })
+        srollTo(MIN_TRANSLATION_Y)
     })
     return (
-            <GestureDetector gesture={gesture}>
+        <GestureDetector gesture={gesture}>
 
-                <Animated.View style={[style.bottomSheetContainer, bottomSheetAnim]} className="bg-[#252525] w-screen">
-                    <View className="bg-slate-100 self-center w-1/5 h-2 rounded mt-5" />
-                    <View className="flex-1">
-                        {children}
-                    </View>
-                </Animated.View>
-            </GestureDetector>
+            <Animated.View style={[style.bottomSheetContainer, bottomSheetAnim]} className="bg-[#252525] w-screen">
+                <View className="bg-slate-100 self-center w-1/5 h-2 rounded mt-5" />
+                <View className="flex-1">
+                    {children}
+                </View>
+            </Animated.View>
+        </GestureDetector>
     )
 }
 
